dbcb371c24c1bd867c03d400001f7ebf892eae57
PugaMarcoIT <100373746+PugaMarcoIT@users.noreply.github.com>
2022-05-12 08:45:33 -0500

    Add files via upload

---
+++ prueba.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <stdio.h>
+#include <conio.h>
+char a,b,c,d;
+char opcion;
+float radio, altura;
+//Este programa calcula el volumen de un cilindro
+void main()
+{
+    printf("radio: ");
+    scanf("%f");
+    printf("altura: ");
+    scanf("%f");
+    printf("volumen: ");
+    if(radio + 100 > altura * 200)
+    {
+        printf("hola");
+        if(1 == 1)
+        {
+            while(4 == 4)
+                do
+                {
+                    a = 0;
+                } while(6 == 6);
+        }
+        else if (2 == 2.0)
+        {
+            b = "hola mundo";
+            x = (3 + 5)*d - (10 - 4)/ c;
+        }
+        else if (3 == 3)
+        {
+            c = a;
+        }
+        else
+            scanf("&d");
+
+        }
+        c = a + (b + c);
+        c = a + b*c;
+        b = "hola mundo
+    }
