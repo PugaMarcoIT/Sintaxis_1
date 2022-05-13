1fe5f7ae3ec70259328a3e28777079bc26dcffbf
PugaMarcoIT <100373746+PugaMarcoIT@users.noreply.github.com>
2022-05-13 18:50:01 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Sintaxis_1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Lenguaje a = new Lenguaje("C:\\Users\\marco\\OneDrive\\Escritorio\\Lenguajes y automatas 1\\Sintaxis_1\\suma.cpp");
+
+                a.Programa();
+                /*while(!a.FinArchivo())
+                {
+                    a.NextToken();
+                }*/
+                a.cerrar();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Fin de compilacion");
+            }
+        }
+    }
+}
---
+++ Sintaxis.cs
@@ -0,0 +1,40 @@
+using System;
+
+namespace Sintaxis_1
+{
+    public class Sintaxis : Lexico
+    {
+        public Sintaxis()
+        {
+            NextToken();
+        }
+        public Sintaxis(string nombre) : base(nombre)
+        {
+            NextToken();
+        }
+
+        public void match(String espera)
+        {
+            if (espera == getContenido())
+            {
+                NextToken();
+            }
+            else
+            {
+                throw new Error("Error de sintaxis, se espera un " +espera, log);
+            }
+        }
+
+        public void match(Tipos espera)
+        {
+            if (espera == getClasificacion())
+            {
+                NextToken();
+            }
+            else
+            {
+                throw new Error("Error de sintaxis, se espera un " +espera, log);
+            }
+        }
+    }
+}
